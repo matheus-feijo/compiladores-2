af1d943381dcb90761d8bbc3ab30f96be2ac98a0
matheus <mat.feijo2506@gmail.com>
2022-12-06 12:08:38 -0400

    evoluindo interpretador

--- src/interpretador.js
+++ src/interpretador.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
+import readline from "readline";
 
-let instrucoes;
+const ENTRADA_VALOR_A = 5;
+const ENTRADA_VALOR_C = 20;
 
 fs.readFile('./saida.txt', 'utf-8', (erro, data) => {
     if (erro) {
@@ -14,7 +16,6 @@ fs.readFile('./saida.txt', 'utf-8', (erro, data) => {
     let ponteiro = 0;
     let instrucaoAtual;
 
-
     while (true) {
         instrucaoAtual = instrucoes[ponteiro].split(' ');
 
@@ -27,44 +28,96 @@ fs.readFile('./saida.txt', 'utf-8', (erro, data) => {
             memoria.push(0);
 
         } else if (instrucaoAtual[0] === 'CRVL') {
+            //pilha.push(instrucaoAtual)
+            pilha.push(memoria[instrucaoAtual[1]])
+
 
         } else if (instrucaoAtual[0] === 'ARMZ') {
+            const valorAtual = pilha.pop();
+            memoria[instrucaoAtual[1]] = valorAtual;
+
 
         } else if (instrucaoAtual[0] === 'SOMA') {
+            const value1 = pilha.pop()
+            const value2 = pilha.pop()
+            pilha.push(value1 + value2)
 
         } else if (instrucaoAtual[0] === 'SUBT') {
+            const value1 = pilha.pop()
+            const value2 = pilha.pop()
+            pilha.push(value1 - value2)
 
         } else if (instrucaoAtual[0] === 'DIVI') {
+            const value1 = pilha.pop()
+            const value2 = pilha.pop()
+            pilha.push(value1 / value2)
 
         } else if (instrucaoAtual[0] === 'MULT') {
+            const value1 = pilha.pop()
+            const value2 = pilha.pop()
+            pilha.push(value1 * value2)
 
         } else if (instrucaoAtual[0] === 'INVE') {
+            const value1 = pilha.pop();
+            pilha.push(value1 * (-1));
 
         } else if (instrucaoAtual[0] === 'CRCT') {
+            const value = parseInt(instrucaoAtual[1]);
+            pilha.push(value);
 
         } else if (instrucaoAtual[0] === 'CPME') {
+            const value1 = pilha.pop()
+            const value2 = pilha.pop()
+            pilha.push(value1 < value2)
 
         } else if (instrucaoAtual[0] === 'CPMA') {
+            const value1 = pilha.pop()
+            const value2 = pilha.pop()
+            pilha.push(value1 > value2)
 
         } else if (instrucaoAtual[0] === 'CPIG') {
+            const value1 = pilha.pop()
+            const value2 = pilha.pop()
+            pilha.push(parseInt(value1 === value2))
+
 
         } else if (instrucaoAtual[0] === 'CDES') {
+            const value1 = pilha.pop()
+            const value2 = pilha.pop()
+            pilha.push(value1 != value2)
 
         } else if (instrucaoAtual[0] === 'CPMI') {
+            const value1 = pilha.pop()
+            const value2 = pilha.pop()
+            pilha.push(value1 <= value2)
 
         } else if (instrucaoAtual[0] === 'CMAI') {
+            const value1 = pilha.pop()
+            const value2 = pilha.pop()
+            pilha.push(value1 >= value2)
 
         } else if (instrucaoAtual[0] === 'DSVF') {
+            const value = pilha.pop();
+            if (value === 0) {
+                ponteiro = parseInt(instrucaoAtual[1]);
+            }
 
         } else if (instrucaoAtual[0] === 'DSVI') {
+            ponteiro = parseInt(instrucaoAtual[1]);
 
         } else if (instrucaoAtual[0] === 'LEIT') {
+            pilha.push(ENTRADA_VALOR_A);
+            //pilha.push()
 
         } else if (instrucaoAtual[0] === 'IMPR') {
+            console.log(pilha.pop());
 
         } else if (instrucaoAtual[0] === 'PARA') {
             break;
         }
+
+        console.log("ponteiro ---> ", ponteiro);
+        ponteiro++;
     }
 
 
